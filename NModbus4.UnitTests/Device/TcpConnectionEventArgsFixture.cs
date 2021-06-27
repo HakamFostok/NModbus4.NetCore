5dee6ced6a3e1935d07a33b138d44f43a8485816
Hakan FISTIK <hakam.fstk@gmail.com>
2021-06-27 15:53:07 +0300

    Add unit test

---
+++ NModbus4.UnitTests/Device/TcpConnectionEventArgsFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Modbus.Device;
+using Xunit;
+
+namespace Modbus.UnitTests.Device
+{
+    public class TcpConnectionEventArgsFixture
+    {
+        [Fact]
+        public void TcpConnectionEventArgs_NullEndPoint()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TcpConnectionEventArgs(null));
+        }
+
+        [Fact]
+        public void TcpConnectionEventArgs_EmptyEndPoint()
+        {
+            Assert.Throws<ArgumentException>(() => new TcpConnectionEventArgs(string.Empty));
+        }
+
+        [Fact]
+        public void TcpConnectionEventArgs()
+        {
+            var args = new TcpConnectionEventArgs("foo");
+
+            Assert.Equal("foo", args.EndPoint);
+        }
+    }
+}
---
+++ NModbus4.UnitTests/Message/DiagnosticsRequestResponseFixture.cs
@@ -0,0 +1,18 @@
+﻿using Modbus.Data;
+using Modbus.Message;
+using Xunit;
+
+namespace Modbus.UnitTests.Message
+{
+    public class DiagnosticsRequestResponseFixture
+    {
+        [Fact]
+        public void ToString_Test()
+        {
+            DiagnosticsRequestResponse response;
+
+            response = new DiagnosticsRequestResponse(Modbus.DiagnosticsReturnQueryData, 3, new RegisterCollection(5));
+            Assert.Equal("Diagnostics message, sub-function return query data - {5}.", response.ToString());
+        }
+    }
+}
