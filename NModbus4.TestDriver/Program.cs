59589b342e3785f5d9c2a892a36041c7f6bfc4e3
Hakan FISTIK <hakam.fstk@gmail.com>
2021-11-09 20:23:23 +0300

    Refactoring the program.cs

--- NModbus4.TestDriver/Program.cs
+++ NModbus4.TestDriver/Program.cs
@@ -1,39 +1,28 @@
-﻿namespace NModbus4.TestDriver
+﻿using System;
+using System.Net.Sockets;
+using Modbus.Device;
+
+try
 {
-    using System;
-    using System.Net.Sockets;
-    using Modbus.Device;
+    using TcpClient client = new("127.0.0.1", 502);
+    client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
 
-    class Program
+    var master = ModbusIpMaster.CreateIp(client);
+    // read five input values
+    ushort startAddress = 100;
+    ushort numInputs = 5;
+    bool[] inputs = master.ReadInputs(startAddress, numInputs);
+
+    for (int i = 0; i < numInputs; i++)
     {
-        static void Main(string[] args)
-        {
-            try
-            {
-                using (TcpClient client = new("127.0.0.1", 502))
-                {
-                    client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-
-                    var master = ModbusIpMaster.CreateIp(client);
-                    // read five input values
-                    ushort startAddress = 100;
-                    ushort numInputs = 5;
-                    bool[] inputs = master.ReadInputs(startAddress, numInputs);
-
-                    for (int i = 0; i < numInputs; i++)
-                    {
-                        Console.WriteLine($"Input {(startAddress + i)}={(inputs[i] ? 1 : 0)}");
-                    }
-
-                    while (true)
-                    {
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
+        Console.WriteLine($"Input {(startAddress + i)}={(inputs[i] ? 1 : 0)}");
     }
+
+    while (true)
+    {
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
 }
