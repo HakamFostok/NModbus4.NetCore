db65e65bc9ca2a9cd0f1fe57cd149884ba9bc15d
Hakan FISTIK <hakam.fstk@gmail.com>
2021-11-09 21:57:23 +0300

    Refactoring

--- NModbus4.UnitTests/Data/UshortModbusDataCollectionFixture.cs
+++ NModbus4.UnitTests/Data/UshortModbusDataCollectionFixture.cs
@@ -15,17 +15,11 @@ public class UshortModbusDataCollectionFixture : ModbusDataCollectionFixture<ush
 
         Assert.False(col.Remove(GetNonExistentElement()));
         Assert.True(col.Remove(source[3]));
-
         Assert.Equal(expectedCount, col.Count);
     }
 
-    protected override ushort[] GetArray()
-    {
-        return new ushort[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    }
+    protected override ushort[] GetArray() =>
+        new ushort[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-    protected override ushort GetNonExistentElement()
-    {
-        return 42;
-    }
+    protected override ushort GetNonExistentElement() => 42;
 }
